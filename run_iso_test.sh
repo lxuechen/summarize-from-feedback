3dbd1e1dfe1b1f925f87cd6af113b155169c3a5a
Xuechen Li <lxuechen@gmail.com>
2022-12-23 11:02:34 -0800

    refactor quick script.

--- run_iso_test.sh
+++ run_iso_test.sh
@@ -1,1 +1,6 @@
+# For some reason, after the first run, the MPI breaks interactive slurm job...
+# Quick script to test things out.
+eval "$(conda shell.bash hook)"
+conda activate lxuechen-summ-hf
 pipenv run python iso_test.py --task "oai_test"
+exit
